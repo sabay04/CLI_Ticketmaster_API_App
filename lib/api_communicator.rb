5dbd041fcec40a0b739b07696a792d9639cc4e7d
studioshane <studioshane@gmail.com>
2019-01-21 17:15:27 +0000

    methods added

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,30 +3,35 @@ require 'json'
 require 'pry'
 
 
-def get_from_api
+def get_event_name_from_api
 
-puts "Please enter the name of the artist"
+# puts "Please enter the name of the artist"
+#
+# artist = gets.chomp
+#
+#  puts "Please enter the name of the city"
+#
+#  city = gets.chomp
 
-artist = gets.chomp
-
- puts "Please enter the name of the city"
-
- city = gets.chomp
-
-  response_string = RestClient.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=pQAHDQNADv3ILD6AiszHahtWnN3y3wN7&city=#{city}&size=10&classificationName=music")
+  response_string = RestClient.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=pQAHDQNADv3ILD6AiszHahtWnN3y3wN7&city=london&size=10&classificationName=music")
   response_hash = JSON.parse(response_string)
-      names = response_hash["_embedded"]["events"].map {|events| events["name"]}
 
+  response_hash["_embedded"]["events"].map do |event|
+    puts "Artist:#{event['name']} ,Date: #{event['dates']['start']['localDate']}, Time: #{event['dates']['start']['localTime']}, Venue: #{event['_embedded']['venues'][0]['name']}, Post code: #{event['_embedded']['venues'][0]['postalCode']},City: #{event['_embedded']['venues'][1]['name']}, Genre: #{event['classifications'][0]['genre']['name']}"
 
-
+      # names = response_hash["_embedded"]["events"].map {|events| events["name"]}
     # response_hash.each do |key, value|
     #   puts key
-    # end
-    names.select do |name|
-      if name.include?(artist)
-        puts name
-      end
+
+
     end
   end
 
-get_from_api
+    # def search_by_name
+    #    names.find do |name|
+    #   if name.include?(artist)
+    #     puts name
+    #   end
+    # end
+
+get_event_name_from_api
